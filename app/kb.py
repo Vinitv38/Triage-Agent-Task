6238216a3b58680c42adc7bc2fceba48c192067c
Vinitv38 <vinitlathiwalav38@gmail.com>
2025-11-20 21:15:07 +0530

    added health

--- app/kb.py
+++ app/kb.py
@@ -6,7 +6,7 @@ from chromadb.config import Settings
 from .models import KBEntry
 import os
 
-KB_PATH = os.path.join(os.path.dirname(__file__), "data", "knowledge_base.json")
+KB_PATH = "data/knowledge_base.json"
 
 class KnowledgeBase:
     def __init__(self, path: Path = KB_PATH):
